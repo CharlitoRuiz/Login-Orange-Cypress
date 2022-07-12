03e8a82ed48e59ea599c895434d97e1bcb641276
Charlito <cruizfreak@hotmail.com>
2022-07-11 18:45:15 -0600

    ejemplo de automatizacion login de orange

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,20 @@
+///<reference types="cypress" />
+import datos from '../fixtures/datos.json';
+
+/* A variable declaration. */
+let userName = datos.userName, password = datos.password, searchUser = datos.searchUser;
+
+describe('empty spec', () => {
+  it('Ingresar al sitio', () => {
+        // Command de cypress
+    cy.ingresarURL('https://opensource-demo.orangehrmlive.com/');
+    cy.login(userName, password);
+
+    cy.get('[href="http://www.orangehrm.com/"] > img').should('be.visible');
+    cy.get('#menu_admin_viewAdminModule > b').click();
+    cy.get('#searchSystemUser_userName').type(searchUser);
+    cy.get('#searchBtn').click();
+    cy.get('td').should('contain', searchUser);
+    cy.get('.odd > :nth-child(4)').should('not.have.value', "Floris Hooijmans");
+  })
+})
